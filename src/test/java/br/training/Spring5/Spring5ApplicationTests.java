9b4778b87ecd3bf966068d0d9842a87e0f6edbc9
gustavonvp <gustavonvp@gmail.com>
2021-07-16 07:02:47 -0300

    1 commit

---
+++ src/test/java/br/training/Spring5/Spring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.training.Spring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
