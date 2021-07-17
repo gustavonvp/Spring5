1ebf062a8c2a625e26d975cce7aeb878dcbbb0bb
gustavonvp <gustavonvp@gmail.com>
2021-07-17 04:37:33 -0300

    Adding Controller Author closes #6

--- src/main/java/br/training/Spring5/controller/AuthorController.java
+++ src/main/java/br/training/Spring5/controller/AuthorController.java
@@ -1,7 +1,22 @@
 package br.training.Spring5.controller;
 
+import br.training.Spring5.repository.AuthorRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
 }
